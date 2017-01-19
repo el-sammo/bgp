9a6650fab22d7025cfe57862816c4c5eaeae19db
Jon Barrett <jonab1974@gmail.com>
2017-01-19 22:30:56 +0000

    debugged export

--- utilities/export.js
+++ utilities/export.js
@@ -1,7 +1,16 @@
 db = new Mongo().getDB('popcorn');
 
-var cursor = db.popcorn.find({});
-while(cursor.hasNext()) {
-	var popcorn = cursor.next();
-print("'"+popcorn._id.valueOf()+"','"+popcorn.name+"','"+popcorn.description+"'");
+var cursorA = db.popcorn.find({}).limit(7).skip(60);
+while(cursorA.hasNext()) {
+	var popcorn = cursorA.next();
+	var categoryId = ObjectId(popcorn.category);
+	var cursorB = db.categories.find({_id: categoryId});
+	while(cursorB.hasNext()) {
+		var category = cursorB.next();
+		var cursorC = db.options.find({popcornId: popcorn._id.valueOf()});
+		while(cursorC.hasNext()) {
+			var option = cursorC.next();
+print("\""+option._id.valueOf()+"\",\""+popcorn.name+"\",\""+popcorn.description+"\",\""+category.name+"\",\""+option.name+"\",\""+option.price+"\"");
+		}
+	}
 }
